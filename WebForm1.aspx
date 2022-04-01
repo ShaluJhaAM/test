76e385f8655b2fed46dcc6d1001cbb91242b3c99
ShaluJhaAM <102797948+ShaluJhaAM@users.noreply.github.com>
2022-04-01 16:03:26 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+ <%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="cc1" %>  
+        
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblDate" runat="server">Enter Date</asp:Label>
+      <asp:TextBox ID="txtDate" runat="server" />
+ <asp:ImageButton ID="ImageButton1" runat="server" Height="17px"
+            ImageUrl="~/image/calender.jpg" onclick="ImageButton1_Click" Width="21px" />
+<asp:Calendar ID="Calendar1" runat="server"
+            onselectionchanged="Calendar1_SelectionChanged" Visible="False">
+        </asp:Calendar>
+            <br />
+             <asp:Label ID="lblEnergyType" runat="server">Select Energy Type</asp:Label>
+            <asp:DropDownList ID="ddlEnergyType" runat="server">
+                <asp:ListItem  Text="Gas" Value="Gas" Enabled="true"></asp:ListItem>
+                <asp:ListItem  Text="Electricity" Value="Electricity"></asp:ListItem>
+            </asp:DropDownList>
+            <br />
+             <asp:Label ID="lblPrice" runat="server">Price</asp:Label>
+            <asp:TextBox ID="txtPrice" runat="server">
+
+            </asp:TextBox>
+            <asp:RegularExpressionValidator ID="RegularExpressionValidator1" ControlToValidate="txtPrice" runat="server" ErrorMessage="Only Numbers allowed" ValidationExpression="\d+"></asp:RegularExpressionValidator>
+
+            <br />
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+
+        </div>
+
+         <div style="margin-left:10px;margin-top:10px">  
+     <asp:GridView ID="GridView1" AutoGenerateColumns="false" runat="server" CellPadding="4"  
+        ForeColor="#333333" GridLines="None">  
+        <AlternatingRowStyle BackColor="White" />  
+        <Columns>  
+            <asp:BoundField HeaderStyle-Width="120px" HeaderText="Date" DataField="Date" />  
+            <asp:BoundField HeaderStyle-Width="120px" HeaderText="Energy Type" DataField="EnergyType" />  
+            <asp:BoundField HeaderStyle-Width="120px" HeaderText=" Price" DataField="Price" />  
+             
+        </Columns>  
+        <EditRowStyle BackColor="#2461BF" />  
+        <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />  
+        <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />  
+        <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />  
+        <RowStyle BackColor="#EFF3FB" />  
+        <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />  
+        <SortedAscendingCellStyle BackColor="#F5F7FB" />  
+        <SortedAscendingHeaderStyle BackColor="#6D95E1" />  
+        <SortedDescendingCellStyle BackColor="#E9EBEF" />  
+        <SortedDescendingHeaderStyle BackColor="#4870BE" />  
+    </asp:GridView>  
+    </div>  
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+     
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                AddDefaultFirstRecord();
+            }
+        }
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            Calendar1.Visible = true;
+        }
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+
+            txtDate.Text = Calendar1.SelectedDate.ToShortDateString();
+
+           // dt.ToString("dd/MM/yyyy");
+            Calendar1.Visible = false;
+        }
+        private void AddDefaultFirstRecord()
+        {
+            //creating dataTable   
+            DataTable dt = new DataTable();
+            DataRow dr;
+            dt.TableName = "Energy Selling";
+            dt.Columns.Add(new DataColumn("Date", typeof(string)));
+            dt.Columns.Add(new DataColumn("EnergyType", typeof(string)));
+            dt.Columns.Add(new DataColumn("Price", typeof(string)));
+           
+            dr = dt.NewRow();
+            dt.Rows.Add(dr);
+            //saving databale into viewstate   
+            ViewState["EnergySelling"] = dt;
+            //bind Gridview  
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+        private void AddNewRecordRowToGrid()
+        {
+            // check view state is not null  
+            if (ViewState["EnergySelling"] != null)
+            {
+                //get datatable from view state   
+                DataTable dtCurrentTable = (DataTable)ViewState["EnergySelling"];
+                DataRow drCurrentRow = null;
+
+                if (dtCurrentTable.Rows.Count > 0)
+                {
+
+                    for (int i = 1; i <= dtCurrentTable.Rows.Count; i++)
+                    {
+                        DateTime dateinput =Convert.ToDateTime(txtDate.Text.ToString());
+                     // DateTime dtResult = DateTime.ParseExact(dateinput, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+
+                       
+
+                        DayOfWeek today = dateinput.DayOfWeek;
+
+
+                     
+                            //add each row into data table  
+                            drCurrentRow = dtCurrentTable.NewRow();
+                        drCurrentRow["Date"] = txtDate.Text;
+                        drCurrentRow["EnergyType"] = ddlEnergyType.SelectedValue;
+
+                        if (today == DayOfWeek.Sunday || today == DayOfWeek.Saturday)
+                        {
+                            int discount = Convert.ToInt32(txtPrice.Text) * 10 / 100;
+                            drCurrentRow["Price"] = Convert.ToInt32(txtPrice.Text)-discount+" -Discount ";
+
+                        }
+                        else
+                        {
+                            drCurrentRow["Price"] = txtPrice.Text;
+                        }
+                        
+                      
+
+
+                    }
+                    //Remove initial blank row  
+                    if (dtCurrentTable.Rows[0][0].ToString() == "")
+                    {
+                        dtCurrentTable.Rows[0].Delete();
+                        dtCurrentTable.AcceptChanges();
+
+                    }
+
+                    //add created Rows into dataTable  
+                    dtCurrentTable.Rows.Add(drCurrentRow);
+                    //Save Data table into view state after creating each row  
+                    ViewState["EnergySelling"] = dtCurrentTable;
+                    //Bind Gridview with latest Row  
+                    GridView1.DataSource = dtCurrentTable;
+                    GridView1.DataBind();
+                    txtDate.Text = "";
+                    txtPrice.Text = "";
+                }
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            AddNewRecordRowToGrid();
+        }
+    }
+}
